64dbf2f930c8ae9f780c8813f13bfee8816c1c94
yura.cheglakov <yura.cheglakov@gmail.com>
2024-09-22 22:45:11 +0300

    added main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from webbrowser import Chrome
+from time import sleep
 
-from selene import browser, be, have, Config
-from selene.support import by
+import pytest
+from selene.api import *
 
-browser.open('https://ya.ru')
-if browser.element(by.text('Принять все')).matching(be.visible):
-   browser.element(by.text('Принять все')).click()
-browser.element('[name="text"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search-result"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+@pytest.fixture
+def size_browser():
+   browser.driver().set_window_size(300, 400)
+   yield
+   browser.close()
+
+@pytest.fixture
+def open_browser():
+   browser.open_url('https://google.com.ru')
+   if browser.element(by.text('Принять все')).matching(be.visible):
+      browser.element(by.text('Принять все')).click()
+
+def test_needle_exist(open_browser,size_browser):
+   browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+   browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
+def test_no_result(open_browser):
+   browser.element('[name="q"]').should(be.blank).type('rsedgfhgjhkgfdgserwaasrdfhgjkgfddasfdgfhgjhkjgfdsafghjkg').press_enter()
+   browser.element('[id="botstuff"]').should(have.text(' ничего не найдено. '))
